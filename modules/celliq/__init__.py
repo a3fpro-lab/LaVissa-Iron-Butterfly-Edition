c01d3c794b4619a2b597c31b80c7cecbfecd7eac
Inkwon Song Jr. <a3fpro@gmail.com>
2025-11-30 17:43:51 -0600

    Create __init__.py

---
+++ modules/celliq/__init__.py
@@ -0,0 +1,13 @@
+"""
+CellIQ Module
+============
+
+Entropy-based biology:
+
+- Cancer and misfolding as local minima of E_TRP
+- Metabolic collapse / recovery trajectories
+- Therapeutic control u(t): CRISPR, small molecules, neuromodulation
+
+This module will contain models and experiments for TRP landscapes
+over biological state spaces.
+"""
